814a7443e7cd37c40164d28c681c3b50b924604e
Andrew Wong <andrewsulliwong@gmail.com>
2017-07-08 13:07:56 -0400

    package

---
+++ app/components/Addtask.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+//import other components
+
+class Addtask extends Component {
+  constructor() {
+    super();
+    this.state = {
+      taskToAdd
+    };
+
+    }
+}
