0b125c10d4fc666d480acde9ca83b34bdea82222
dwyanalford <dwyan.alford@gmail.com>
2017-07-13 13:58:52 -0400

    set up todo home page and todopanel

--- app/components/Todohome.js
+++ app/components/Todohome.js
@@ -1,16 +1,56 @@
-import React from 'react'
+import React, { Component } from "react";
+import TodoItem from "./sub/TodoItem";
+import Schedule from "./sub/Schedule";
+import TodoPanel from "./sub/TodoPanel";
+import API from "../utils/api";
 
-// This is a stateless functional component - no states just props
-// 'this' keyword will not be available so just need 'props'
-// also render() method is not necessary to display UI
-function Home (props) {
+class Home extends Component {
+  constructor() {
+    super();
+    this.state = { 
+      todos: [],
+      todoSchedule: []
+    };
 
+    this.getTasks = this.getTasks.bind(this);
+  }
+
+  componentDidMount() {
+    this.getTasks();
+  }
+
+  getTasks() {
+    //build axios methods
+    API.getTasks().then((res) => {
+      this.setState({ todos: res.data });
+    });
+  }
+
+  getSchedule() {
+    API.getSchedule().then((res) => {
+      this.setState({ todoSchedule: res.data})
+    });
+  }
+
+  render() {
     return (
-      <div className="background-1">
-        <h1>Home Page - TodoHome</h1>
+      <div className="container">
+        <div className="row">
+          <TodoPanel
+            tasks={this.state.todos}
+            getTasks={this.getTasks}
+          />
+        </div>
+        <hr />
+        <div className="row">
+          <Schedule 
+            tasks={this.state.todoSchedule}
+            getSchedule={this.getSchedule}
+          />
+        </div>
       </div>
-    )
-  
+    );
+  }
 }
 
-export default Home;
+export default Home
